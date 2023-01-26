66075be8e0c32dfa5b071d74552779dd3f7ae01d
Shivam <shivam2728@gmail.com>
2023-01-26 21:26:20 +0530

    done

--- Sorting Algorithms/Cycle sort/Cyclic_Sort.java
+++ Sorting Algorithms/Cycle sort/Cyclic_Sort.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class Cyclic_Sort {
     public static void main(String[] args) {
-        int[] arr={4,3,1,2};
+        int[] arr={4,3,1,2};  //for arr 1 to n only...
         sort(arr);
         System.out.println(Arrays.toString(arr));
     }
