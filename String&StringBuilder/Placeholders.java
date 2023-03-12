c00048c18ec8a0ade493ef440967d21a72db58bf
Shivam <shivam2728@gmail.com>
2023-03-12 21:24:02 +0530

    new

---
+++ String&StringBuilder/Placeholders.java
@@ -0,0 +1,16 @@
+public class Placeholders {
+    public static void main(String[] args) {
+        float a = 3.458829f;
+        System.out.println("Orignal value is " + a);
+        System.out.printf("Formated value is %3f \n",a);
+        System.out.printf("Hi my name is %s and i am a %s student. i am in %d nd year.\n","Shivam","BscIT",2);
+    }
+    /* 
+    1. %d: Placeholder for integer values
+    2. %f: Placeholder for floating-point values (float and double)
+    3. %s: Placeholder for string values
+    4. %c: Placeholder for character values
+    5. %b: Placeholder for boolean values
+    6. %n: Placeholder for a new line (same as "\n")
+    */
+}
