28a193161593424bd16058e4f156f6320d76dfce
Shivam <shivam2728@gmail.com>
2023-04-14 11:48:26 +0530

    patters added

---
+++ Patterns/Pattern1.java
@@ -0,0 +1,21 @@
+// Pattern:- 
+
+//         *
+//         **
+//         ***
+//         ****
+
+
+public class Pattern1
+{
+    public static void main(String[] args) {
+        for(int row=1;row<=4;row++)
+        {
+            for(int col=0;col<row;col++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
