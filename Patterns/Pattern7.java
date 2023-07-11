e087af536c1dcc557b08ad8ed511710e525f51ea
Shivam <shivam2728@gmail.com>
2023-07-11 17:24:16 +0530

    ..

--- Patterns/Pattern7.java
+++ Patterns/Pattern7.java
@@ -1,8 +1,8 @@
 // Pattern7:-
-//          1
-//         212
-//        32123
-//       4321234
+//      1
+//     212
+//    32123
+//   4321234
 
 public class Pattern7 {
         public static void main(String[] args) {
@@ -11,15 +11,15 @@ public class Pattern7 {
             {
                 for(int space = row;space <n;space++)
                 {
-                    System.out.print(" ");
+                    System.out.print("  ");
                 }
                 for(int numleft = row;numleft>0;numleft--)
                 {
-                    System.out.print(numleft);
+                    System.out.print(" "+numleft);
                 }
                 for(int numright=2;numright<=row;numright++)
                 {
-                    System.out.print(numright);
+                    System.out.print(" "+numright);
                 }
                 System.out.println();
             }
