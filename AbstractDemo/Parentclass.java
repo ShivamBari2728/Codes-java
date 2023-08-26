68da777f32a3d8b72d9b2d2292d70e556c875640
Shivam <shivam2728@gmail.com>
2023-08-26 17:04:32 +0530

    abstract

---
+++ AbstractDemo/Parentclass.java
@@ -0,0 +1,11 @@
+package Java.AbstractDemo;
+
+public abstract class Parentclass {
+    abstract void carrer();
+    abstract void partner();
+    static void sayhello()
+    {
+        System.out.println("hiii");
+    }
+  
+}
